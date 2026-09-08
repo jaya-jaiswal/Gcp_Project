ee3bbc2d26609678bdf236b15e465dce77552a00
sid279 <ajitkumar212@gmail.com>
2026-09-08 18:54:08 +0530

    deploy

--- retail-dataset/dags/retail_pipeline.py
+++ retail-dataset/dags/retail_pipeline.py
@@ -1,17 +1,41 @@
 from airflow import DAG
+from airflow.providers.google.cloud.operators.dataflow import DataflowCreatePythonJobOperator
 from airflow.providers.google.cloud.operators.bigquery import BigQueryInsertJobOperator
 from datetime import datetime
 
-with DAG("retail_pipeline", start_date=datetime(2024,1,1), schedule_interval=None, catchup=False) as dag:
+PROJECT_ID = "project-d0445eef-b5cb-453b-a9a"
+REGION = "us-central1"
+BUCKET = "us-central1-airflow3-cc018626-bucket"
 
+with DAG(
+    dag_id="retail_end_to_end_pipeline",
+    start_date=datetime(2024, 1, 1),
+    schedule=None,
+    catchup=False
+) as dag:
+
+    # 🚀 1. Dataflow Job
+    run_dataflow = DataflowCreatePythonJobOperator(
+        task_id="run_dataflow",
+        py_file=f"gs://{BUCKET}/dataflow/job.py",
+        location=REGION,
+        project_id=PROJECT_ID,
+        options={
+            "temp_location": f"gs://{BUCKET}/temp",
+        }
+    )
+
+    # 🧠 2. Run Stored Procedure
     run_sp = BigQueryInsertJobOperator(
         task_id="run_stored_procedure",
         configuration={
             "query": {
-                "query": "CALL retail_dataset.sp_clean_orders();",
+                "query": "CALL `retail_dataset.sp_clean_orders`()",
                 "useLegacySql": False,
             }
-        }
+        },
+        location=REGION,
     )
 
-    run_sp
+    # 🔗 Dependency
+    run_dataflow >> run_sp
