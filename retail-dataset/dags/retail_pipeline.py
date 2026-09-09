4d7aa30df36adccb40d68bdf9a2727f4e29701d6
sid279 <ajitkumar212@gmail.com>
2026-09-09 20:40:15 +0530

    changeed schedule time

--- retail-dataset/dags/retail_pipeline.py
+++ retail-dataset/dags/retail_pipeline.py
@@ -25,10 +25,8 @@ with DAG(
     parameters={
         "inputFilePattern": "gs://project-d0445eef-b5cb-453b-a9a-landing-bucket/*.csv",
         "outputTable": f"{PROJECT_ID}:retail_dataset.raw_orders",
-        "schemaJSONPath": "gs://project-d0445eef-b5cb-453b-a9a-landing-bucket/schema.json",
-        "bigQueryLoadingTemporaryDirectory": "gs://us-central1-airflow3-cc018626-bucket/temp",
-        "writeDisposition": "WRITE_APPEND",
-        "skipLeadingRows": "1"
+        "JSONPath": "gs://project-d0445eef-b5cb-453b-a9a-landing-bucket/schema.json",
+        "bigQueryLoadingTemporaryDirectory": "gs://us-central1-airflow3-cc018626-bucket/temp"
     },
     location=REGION,
     project_id=PROJECT_ID
