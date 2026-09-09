03ec4a613d219a82356d3d108c48214a97e162a8
sid279 <ajitkumar212@gmail.com>
2026-09-10 00:18:03 +0530

    changeed schedule time

--- retail-dataset/dags/retail_pipeline.py
+++ retail-dataset/dags/retail_pipeline.py
@@ -20,13 +20,15 @@ with DAG(
     # 🚀 Dataflow Job (PERMANENT FIX - VERSION LOCKED)
     run_dataflow = DataflowTemplatedJobStartOperator(
     task_id="run_dataflow",
-    template="gs://dataflow-templates-us-central1/2023-12-12-00_RC00/GCS_Text_to_BigQuery",
+    template="gs://dataflow-templates-us-central1/2023-12-12-00_RC00/GCS_CSV_to_BigQuery",
     parameters={
-        "inputFilePattern": "gs://project-d0445eef-b5cb-453b-a9a-landing-bucket/data/*.csv",
-        "outputTable": f"{PROJECT_ID}:retail_dataset.raw_orders",
-        "JSONPath": "gs://project-d0445eef-b5cb-453b-a9a-landing-bucket/schema.json",
-        "bigQueryLoadingTemporaryDirectory": "gs://us-central1-airflow3-cc018626-bucket/temp"
-},
+    "inputFilePattern": "gs://project-d0445eef-b5cb-453b-a9a-landing-bucket/data/*.csv",
+    "outputTable": f"{PROJECT_ID}:retail_dataset.raw_orders",
+    "schemaJSONPath": "gs://project-d0445eef-b5cb-453b-a9a-landing-bucket/schema.json",
+    "bigQueryLoadingTemporaryDirectory": "gs://us-central1-airflow3-cc018626-bucket/temp",
+    "skipLeadingRows": "1",
+    "fieldDelimiter": ","
+}
     location=REGION,
     project_id=PROJECT_ID,
 
